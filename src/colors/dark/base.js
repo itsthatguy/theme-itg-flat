1d131fbde91a42eadba88af8f648faff356cc405
Kevin Altman <kevin@itsthatguy.com>
2015-04-17 14:09:20 -0600

    removes extra whitespace to prevent superfluous git diffs

--- src/colors/dark/base.js
+++ src/colors/dark/base.js
@@ -7,10 +7,10 @@ module.exports = {
   red               : defaultColors.red,
   yellow            : "[240, 200, 80, 255]",
   darkYellow        : "[205, 135, 0]",
-  dark              : "[91,  84,  81, 255]",
+  dark              : "[91, 84, 81, 255]",
   white             : "[255, 255, 255, 200]",
   lightWhite        : "[180, 180, 180]",
-  primaryDark       : "[65,  69,  78]",
+  primaryDark       : "[65, 69, 78]",
   primaryColor      : defaultColors.red,
   lightPrimaryColor : defaultColors.lightPrimaryColor
 };
