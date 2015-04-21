79e1499de4f3d49bd62788ed426d6b13fa87a9fa
Kevin Altman <kevin@itsthatguy.com>
2015-04-21 18:57:36 -0400

    Updates tests
    * better feedback for JSON validation
    
    * basic dark merge comparison

--- test/index.js
+++ test/index.js
@@ -1,29 +1,57 @@
 var app      = require('../Gulpfile.js'),
-    fs       = require('fs'),
+    async    = require('async'),
+    glob     = require('glob'),
     path     = require('path'),
     gulp     = require('gulp');
+
 
 require.extensions['.sublime-theme'] = require.extensions['.json'];
 var expect = require("chai").expect;
 var tmpDir = path.join(process.cwd() + '/test/tmp/');
+var templatesDir = path.join(process.cwd() + '/test/templates/');
+var srcDir = path.join(process.cwd() + '/src/');
 
 describe('Compile', function() {
   before(function(done) {
-    var config = {
-      mustache: { dest: tmpDir }
-    };
-
+    var config = { mustache: { dest: tmpDir } };
     app.exec(config, done);
   });
 
-  it('should generate valid files', function(done) {
-    fs.readdir(tmpDir, function(err, files) {
-      files.forEach(function(file) {
-        require(tmpDir + file);
-      });
+  describe('should generate valid JSON', function() {
+    var files = glob.sync(tmpDir + '**/*.sublime-theme');
 
+    async.each(files, function(file) {
+      it(path.basename(file), function(done) {
+        require(file);
+        done();
+      });
+    });
+
+  });
+});
+
+var compareMerge = function(dir) {
+  var colorsFn = require(srcDir + 'colors/colors.js');
+      base     = require(srcDir + 'colors/' + dir + '/base.js'),
+      merged   = colorsFn(base, {}),
+      template = require(templatesDir + dir + '.js');
+
+  return {merged: merged, template: template};
+
+};
+
+describe('src', function() {
+  describe('merges base data as expected', function() {
+    it('dark', function(done) {
+      var data = compareMerge('dark');
+      expect(data.merged).to.deep.equal(data.template);
+      done();
+    });
+
+    it('light', function(done) {
+      var data = compareMerge('light');
+      expect(data.merged).to.deep.equal(data.template);
       done();
     });
   });
-
 });
