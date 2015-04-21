de9fc58f0ab48b007de0411179ea1a58a5fce025
Kevin Altman <kevin@itsthatguy.com>
2015-04-21 11:05:03 -0400

    Adds test to validate json

--- test/index.js
+++ test/index.js
@@ -1,16 +1,29 @@
-var app = require('../Gulpfile.js'),
-    path = require('path'),
-    gulp = require('gulp');
+var app      = require('../Gulpfile.js'),
+    fs       = require('fs'),
+    path     = require('path'),
+    gulp     = require('gulp');
 
+require.extensions['.sublime-theme'] = require.extensions['.json'];
 var expect = require("chai").expect;
+var tmpDir = path.join(process.cwd() + '/test/tmp/');
 
 describe('Compile', function() {
-  it('should generate files', function(done) {
+  before(function(done) {
     var config = {
-      mustache: {
-        dest: path.join(process.cwd() + '/test/tmp/')
-      }
+      mustache: { dest: tmpDir }
     };
+
     app.exec(config, done);
   });
+
+  it('should generate valid files', function(done) {
+    fs.readdir(tmpDir, function(err, files) {
+      files.forEach(function(file) {
+        require(tmpDir + file);
+      });
+
+      done();
+    });
+  });
+
 });
